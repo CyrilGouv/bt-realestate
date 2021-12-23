6777e87ba6854a716f19480ec6b998059ee5624a
Cyril Gouv <hello@cyrilgouv.com>
2021-12-23 11:25:07 +0100

    creating listings app

--- btre/urls.py
+++ btre/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
