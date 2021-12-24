56c7c103cb414b5fe5231fd111848059a259df7c
Cyril Gouv <hello@cyrilgouv.com>
2021-12-24 16:23:02 +0100

    Get All Data for About

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from listings.models import Listing
+from realtors.models import Realtor
 
 
 def index(request):
@@ -13,4 +14,12 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    realtor_mvp = Realtor.objects.filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'realtor_mvp': realtor_mvp
+    }
+
+    return render(request, 'pages/about.html', context)
