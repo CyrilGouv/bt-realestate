4a49c1a5199dce042ca4443369ae92f22fe19839
Cyril Gouv <hello@cyrilgouv.com>
2021-12-24 10:25:11 +0100

    Create Listing & Realtor Models

--- listings/urls.py
+++ listings/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import listings
+from .views import listings, search, detail
 
 
 urlpatterns = [
     path('', listings, name='listings'),
+    path('search/', search, name='search'),
+    path('<int:id>/', detail, name='detail'),
 ]
--- listings/views.py
+++ listings/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 
 def listings(request):
     return render(request, 'listings/listings.html')
+
+
+
+def detail(request):
+    return render(request, 'listings/listing.html')
+
+
+
+def search(request):
+    return render(request, 'listings/search.html')
