6206e60353215f1bbad683514f0c0c4ff18c6a38
Cyril Gouv <hello@cyrilgouv.com>
2021-12-24 18:47:57 +0100

    Add Choices Files to Listings for Fields

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import bedroom_choices, price_choices, state_choices
 
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
-        'listings': listings
+        'listings': listings,
+        'bedrooms': bedroom_choices,
+        'prices': price_choices,
+        'states': state_choices,
     }
 
     return render(request, 'pages/index.html', context)
