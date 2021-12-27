770a74d4735af4d64a21bc20bd9d17c823b233f0
Cyril Gouv <hello@cyrilgouv.com>
2021-12-27 11:58:42 +0100

    Restricted Dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages, auth
 from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -58,6 +59,7 @@ def login(request):
     return render(request, 'accounts/login.html')
 
 
+@login_required
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
 
